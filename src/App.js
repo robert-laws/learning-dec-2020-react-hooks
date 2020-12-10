eabca854940f78e1c3f8a68945a94e98f522e92e
Robert Laws <roblaws@me.com>
2020-12-10 14:58:56 +0300

    basics of object and array destructuring

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.scss';
+import Heading from './components/Heading';
+import List from './components/List';
 
 function App() {
-  return <div className='App'>App</div>;
+  return (
+    <div className='App'>
+      <Heading headingType='h2' headingText='Welcome to the App' />
+      <List items={['hat', 'bat', 'mat', 'pencil']} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Heading.js
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Heading = ({ headingType, headingText }) => {
+  const getHeading = (heading, text) => {
+    switch (heading) {
+      case 'h1':
+        return <h1>{text}</h1>;
+      case 'h2':
+        return <h2>{text}</h2>;
+      case 'h3':
+        return <h3>{text}</h3>;
+      case 'h4':
+        return <h4>{text}</h4>;
+      case 'h5':
+        return <h5>{text}</h5>;
+      case 'h6':
+        return <h6>{text}</h6>;
+      default:
+        return <h1>{text}</h1>;
+    }
+  };
+
+  return getHeading(headingType, headingText);
+};
+
+export default Heading;
